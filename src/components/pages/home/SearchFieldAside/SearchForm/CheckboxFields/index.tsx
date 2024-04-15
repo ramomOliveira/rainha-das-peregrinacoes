ef8fe0138cb15ae3f977b7ffd90dcec77947ce31
Ramom Oliveira <ra-mom2000@hotmail.com>
2024-04-15 20:20:45 -0300

    improvement in checkbox input

--- src/components/pages/home/SearchFieldAside/SearchForm/CheckboxFields/index.tsx
+++ src/components/pages/home/SearchFieldAside/SearchForm/CheckboxFields/index.tsx
@@ -1,21 +1,33 @@
+'use client'
+
+import { Check } from '@phosphor-icons/react'
 import { Container } from './style'
 
 export default function CheckboxFields() {
   return (
     <Container>
       <p>Tipo de viagem:</p>
-      <label htmlFor="">
-        <input type="checkbox" />
+      <label htmlFor="nacional">
+        <input type="checkbox" id="nacional" />
+        <span>
+          <Check weight="bold" />
+        </span>
         Nacional
       </label>
 
-      <label htmlFor="">
-        <input type="checkbox" />
+      <label htmlFor="internacional">
+        <input type="checkbox" id="internacional" />
+        <span>
+          <Check weight="bold" />
+        </span>
         Internacional
       </label>
 
-      <label htmlFor="">
-        <input type="checkbox" />
+      <label htmlFor="promocionais">
+        <input type="checkbox" id="promocionais" />
+        <span>
+          <Check weight="bold" />
+        </span>
         Promocionais
       </label>
     </Container>
--- src/components/pages/home/SearchFieldAside/SearchForm/CheckboxFields/style.ts
+++ src/components/pages/home/SearchFieldAside/SearchForm/CheckboxFields/style.ts
@@ -15,6 +15,31 @@ export const Container = styled.div`
     display: flex;
     align-items: center;
     gap: 8px;
+
+    > input {
+      opacity: 0;
+      position: absolute;
+    }
+    > span {
+      display: inline-block;
+      width: 16px;
+      height: 16px;
+      border: 1px solid white;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      padding: 1px;
+      > svg {
+        display: none;
+      }
+    }
+    > input:checked + span {
+      background-color: white;
+      color: #015287;
+      > svg {
+        display: block;
+      }
+    }
   }
 
   @media (min-width: 769px) {
